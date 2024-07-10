c1073a88b3e58a57d1ca48c5e18d25531a71e925
Miguel Jerónimo <mjeronimo@frd.org.mx>
2024-07-10 11:32:24 -0600

    first configuration to springboot project

---
+++ src/main/kotlin/com/miguel/tibia_merchants_api/TibiaMerchantsApiApplication.kt
@@ -0,0 +1,11 @@
+package com.miguel.tibia_merchants_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TibiaMerchantsApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TibiaMerchantsApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/miguel/tibia_merchants_api/TibiaMerchantsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.miguel.tibia_merchants_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TibiaMerchantsApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
